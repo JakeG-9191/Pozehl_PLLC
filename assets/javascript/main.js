ebec8624014a9fddc105076b8ecfeca080e4195c
Jake Garlick <jacobgarlick@msn.com>
2021-12-12 18:24:33 -0700

    added basic css, js and html pages, added fuctionality to choose between pages, added basic nav and footer, began to build out home page

---
+++ assets/javascript/main.js
@@ -0,0 +1,18 @@
+const ALL_CONTENT = ['home_page', 'about_page', 'q_a_page', 'contact_page'];
+
+const showSectionContent = (show) => {
+    const contentToRender = [...ALL_CONTENT];
+    if (show && contentToRender.includes(show)) {
+        document.getElementById(show).style.display = 'block';
+        const elementIndex = contentToRender.indexOf(show);
+        contentToRender.splice(elementIndex, 1);
+        contentToRender.forEach((item) => {
+            document.getElementById(item).style.display = 'none';
+        });
+    }
+    return false;
+};
+
+showSectionContent('home_page');
+
+console.log('global only on first');
