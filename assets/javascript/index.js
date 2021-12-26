3bfb7a7650900f11f23952c6558dac4009b2072b
JakeG-9191 <jacobgarlick@msn.com>
2021-12-26 11:03:44 -0700

    added link for linkedin icon, added placeholder text for q and a, added embedded google map for contact page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,16 +1,17 @@
-const ALL_CONTENT = ['home_page', 'about_page', 'q_a_page', 'contact_page'];
+const ALL_CONTENT = ["home_page", "about_page", "q_a_page", "contact_page"];
 
 const showSectionContent = (show) => {
-    const contentToRender = [...ALL_CONTENT];
-    if (show && contentToRender.includes(show)) {
-        document.getElementById(show).style.display = 'block';
-        const elementIndex = contentToRender.indexOf(show);
-        contentToRender.splice(elementIndex, 1);
-        contentToRender.forEach((item) => {
-            document.getElementById(item).style.display = 'none';
-        });
-    }
-    return false;
+  const contentToRender = [...ALL_CONTENT];
+  if (show && contentToRender.includes(show)) {
+    document.getElementById(show).style.display = "block";
+    const elementIndex = contentToRender.indexOf(show);
+    contentToRender.splice(elementIndex, 1);
+    contentToRender.forEach((item) => {
+      document.getElementById(item).style.display = "none";
+    });
+  }
+  window.scrollTo(0, 0);
+  return false;
 };
 
-showSectionContent('home_page');
+showSectionContent("home_page");
